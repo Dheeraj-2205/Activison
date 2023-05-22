d4a59bd358fa331f44d65d88fbcef44c07663fa1
Dheeraj-2205 <djsays124@gmail.com>
2023-05-23 01:30:46 +0530

    adding the files

---
+++ rockstar/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div></div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
